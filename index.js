481082544210a2d1715a4977176c7ceb15f071c0
Collins Kandie <collinskandie1@gmail.com>
2024-06-27 00:46:38 +0300

    passed all test

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+// Define the saturdayFun function
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define the mondayWork function expression
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Define the wrapAdjective function
+  function wrapAdjective(wrapper = '*') {
+    return function(adjective = 'special') {
+      return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+  }
+  
